b9651df9eab4831e0f90ea91cf79f1090f2a6337
muratakcan <notebookbilisim@gmail.com>
2025-06-15 05:57:08 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-
 from flask import Flask, request, jsonify
 import requests
 
 app = Flask(__name__)
 
-RESEMBLE_API_KEY = "RBMY3xzlld3nyJaWr8FaSwtt"
+RESEMBLE_API_KEY = "RBM9Y3x1ld3ny3aW8FaSWt"
 RESEMBLE_VOICE_UUID = "f4641ad8"
 
 @app.route('/synthesize_audio', methods=['POST'])
@@ -27,11 +26,4 @@ def synthesize_audio():
 
     response = requests.post("https://app.resemble.ai/api/v2/clips", headers=headers, json=payload)
 
-    if response.status_code == 200:
-        audio_url = response.json().get("audio_url")
-        return jsonify({"audio_url": audio_url}), 200
-    else:
-        return jsonify({"error": "Failed to generate audio"}), 500
-
-if __name__ == '__main__':
-    app.run()
+    return jsonify(response.json()), response.status_code
